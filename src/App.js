44a7e7d0cbb1cd22f4e979737ada94ac1f20387c
DaScOrPiO <dunni82@yahoo.com>
2023-02-01 16:17:52 +0100

    FEAT: Auto dark mode implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Homepage from "./pages/homepage";
 import ContactPage from "./pages/contact";
 import Footer from "./components/footer";
@@ -6,14 +6,23 @@ import DarkMode from "./components/DarkMode";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [Dark, setDark] = useState(false);
-  const changeMode = () => {
-    setDark((prev) => !prev);
-    console.log("mode changed");
+  const getcurrentTheme = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const [Dark, setDark] = useState(getcurrentTheme());
+  const changeMode = (e) => {
+    setDark(e.matches);
   };
 
+  useEffect(() => {
+    const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+    darkTheme.addListener(changeMode);
+
+    return () => darkTheme.removeListener(changeMode);
+  }, []);
+
   return (
-    <div id={Dark ? "light" : "dark"} className="light">
+    <div id={Dark ? "dark" : "light"} className="light general">
       <BrowserRouter>
         <DarkMode handleClick={changeMode} dark={Dark} />
         <Routes>
